b765d88aa16af5a409a9fcf875dafc04e00fbb43
anujgupta <anuj.gupta@sdgc.com>
2019-10-16 15:52:31 +0530

    Update sample-sendNotifications.groovy

--- sample-sendNotifications.groovy
+++ sample-sendNotifications.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
 /**
- * Send notifications based on build status string
- */
+* Send notifications based on build status string
+*/
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   buildStatus = buildStatus ?: 'SUCCESS'
@@ -11,10 +11,11 @@ def call(String buildStatus = 'STARTED') {
   def colorName = 'RED'
   def colorCode = '#FF0000'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def attachLog = true
   def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-
+    
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
@@ -22,17 +23,20 @@ def call(String buildStatus = 'STARTED') {
   } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
-  } else {
+  } else if (buildStatus == 'FAILURE') {
     color = 'RED'
     colorCode = '#FF0000'
-  }
-
+} else {
+              color = 'BLUE'
+    colorCode = '#0000FF'
+  } 
   // Send notifications
 
   emailext (
-      to: 'anuj.gupta1000097@homecredit.co.in; pawan.kumar@homecredit.co.in',
+      to: 'in.jenkins@homecredit.co.in',
       subject: subject,
       body: details,
+      attachLog: attachLog,
       recipientProviders: [[$class: 'DevelopersRecipientProvider']]
-    )
+    ) 
 }
